805b173ec43238e5eb95d04ede779e9130b8152f
Tomas Anastacio <tomaslautaroan@gmail.com>
2023-06-03 23:04:51 -0300

    MERN PRACTICE UPLOAD

--- reactjs-5/src/components/Movies.jsx
+++ reactjs-5/src/components/Movies.jsx
@@ -13,7 +13,7 @@ export function Movies() {
                 Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YzNiZjgwYjY3OTI5YTY0YjJmYmJhNDgyOTg0OWE1ZiIsInN1YiI6IjY0N2E4MzUzMGUyOWEyMDBiZjFlNmFhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L1XP6OPqG-fjo_u4qOkWk5pHvOkIimsWa34R7rWAZ9s'
             }
         }
-        fetch("https://api.themoviedb.org/3/movie/popular", options)
+        fetch("https://api.themoviedb.org/3/moviegi/popular", options)
             .then((response) => response.json())
             .then((json) => {
                 let moviesResult = [];
